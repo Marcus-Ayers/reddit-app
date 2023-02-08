023006f256c035f978c910c5d506aeba9f2eb7c0
Marcus Ayers <marcayers27@gmail.com>
2023-02-08 17:30:53 -0500

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_015724) do
+ActiveRecord::Schema.define(version: 2023_02_08_221139) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
